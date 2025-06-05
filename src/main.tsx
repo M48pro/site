4e10c832adc9c2480502a1ce21b5d68f616e1382
M48pro <m48.office@gmail.com>
2025-06-05 15:53:01 +0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // ← должен быть этот импорт
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
